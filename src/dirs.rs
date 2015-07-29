d5716b2544b027449a8ea31e79fac8bdac8e3419
Ben Boeckel <mathstuf@gmail.com>
2015-07-28 22:57:04 -0400

    dirs: move from config
    Also add data directory functions.

---
+++ src/dirs.rs
@@ -0,0 +1,29 @@
+extern crate xdg_basedir;
+use error::FsError;
+
+use std::fs::create_dir_all;
+use std::path::PathBuf;
+
+pub fn dir_config_create(name: &str) -> Result<PathBuf, FsError> {
+    let path: PathBuf = try!(dir_config(name));
+    try!(create_dir_all(path.as_path()));
+    Ok(path)
+}
+
+pub fn dir_config(name: &str) -> Result<PathBuf, FsError> {
+    let mut path: PathBuf = try!(xdg_basedir::get_config_home());
+    path.push(name);
+    Ok(path)
+}
+
+pub fn dir_data_create(name: &str) -> Result<PathBuf, FsError> {
+    let path: PathBuf = try!(dir_data(name));
+    try!(create_dir_all(path.as_path()));
+    Ok(path)
+}
+
+pub fn dir_data(name: &str) -> Result<PathBuf, FsError> {
+    let mut path: PathBuf = try!(xdg_basedir::get_data_home());
+    path.push(name);
+    Ok(path)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod config;
+pub mod dirs;
 pub mod error;
 
 pub mod dbus_listen;
