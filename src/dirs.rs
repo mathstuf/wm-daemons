67cca87af5a54d72299a437a4654174891068652
Ben Boeckel <mathstuf@gmail.com>
2015-07-28 23:50:16 -0400

    dirs: remove unnecessary type specifications

--- src/dirs.rs
+++ src/dirs.rs
@@ -6,25 +6,25 @@ use std::fs::create_dir_all;
 use std::path::PathBuf;
 
 pub fn dir_config_create(name: &str) -> Result<PathBuf, FsError> {
-    let path: PathBuf = try!(dir_config(name));
+    let path = try!(dir_config(name));
     try!(create_dir_all(path.as_path()));
     Ok(path)
 }
 
 pub fn dir_config(name: &str) -> Result<PathBuf, FsError> {
-    let mut path: PathBuf = try!(xdg_basedir::get_config_home());
+    let mut path = try!(xdg_basedir::get_config_home());
     path.push(name);
     Ok(path)
 }
 
 pub fn dir_data_create(name: &str) -> Result<PathBuf, FsError> {
-    let path: PathBuf = try!(dir_data(name));
+    let path = try!(dir_data(name));
     try!(create_dir_all(path.as_path()));
     Ok(path)
 }
 
 pub fn dir_data(name: &str) -> Result<PathBuf, FsError> {
-    let mut path: PathBuf = try!(xdg_basedir::get_data_home());
+    let mut path = try!(xdg_basedir::get_data_home());
     path.push(name);
     Ok(path)
 }
