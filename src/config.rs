f49f77b897638cd282da7d4989785e7d8d2e86a4
Ben Boeckel <mathstuf@gmail.com>
2015-07-28 23:45:05 -0400

    config: use stable function call

--- src/config.rs
+++ src/config.rs
@@ -5,12 +5,14 @@ extern crate config;
 use self::config::reader::from_file;
 use self::config::types::{Config, SettingsList};
 
+use std::fs::metadata;
+
 pub fn load_config(app: &str, file: &str) -> Result<Config, ConfigError> {
     let mut path_buf = try!(dir_config_create(app));
     path_buf.push(file);
     let path = path_buf.as_path();
 
-    if path.exists() {
+    if metadata(path).ok().map_or(false, |m| m.is_file()) {
         let config = try!(from_file(path));
         Ok(config)
     } else {
