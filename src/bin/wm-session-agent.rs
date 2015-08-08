5e55f744c5c5b7698e7877ed8e78a92b032841f6
Ben Boeckel <mathstuf@gmail.com>
2015-08-08 17:52:41 -0400

    wm-session-agent: add arguments

--- src/bin/wm-session-agent.rs
+++ src/bin/wm-session-agent.rs
@@ -1,6 +1,10 @@
 extern crate wm_daemons;
-use wm_daemons::config::load_config;
+use wm_daemons::config::{load_config, load_config_path};
 use wm_daemons::dbus_listen::{CallbackMap, SignalInfo, match_signal};
+
+#[macro_use]
+extern crate clap;
+use clap::{Arg, App};
 
 extern crate config;
 use self::config::types::Config;
@@ -9,6 +13,7 @@ extern crate dbus;
 use self::dbus::{Connection, BusType};
 
 use std::error::Error;
+use std::path::Path;
 
 fn run_program(action: &str, conf: &Config) -> () {
     // TODO
@@ -23,7 +28,32 @@ fn handle_signal(info: &SignalInfo, conf: &Config) -> () {
 }
 
 fn try_main() -> Result<(), Box<Error>> {
-    let conf = try!(load_config("wm-session-agent", "config"));
+    let matches = App::new("wm-session-agent")
+        .version(&crate_version!()[..])
+        .author("Ben Boeckel <mathstuf@gmail.com>")
+        .about("Listens for logind's Lock and Unlock signals")
+        .arg(Arg::with_name("CONFIG")
+                .short("c")
+                .long("config")
+                .help("Path to the configuration value")
+                .takes_value(true))
+        .arg(Arg::with_name("SESSION")
+                .short("s")
+                .long("session")
+                .required(true)
+                .help("Session ID to listen on")
+                .takes_value(true))
+        .get_matches();
+
+    let conf = try!(if matches.is_present("CONFIG") {
+            load_config("wm-session-agent", "config")
+        } else {
+            load_config_path(Path::new(matches.value_of("CONFIG").unwrap()))
+        });
+
+    let sid = matches.value_of("SESSION").unwrap();
+    let spath = format!("/org/freedesktop/login1/session/_{}", sid);
+
     let conn = try!(Connection::get_private(BusType::System));
 
     let cbs: CallbackMap<Config> = vec![
