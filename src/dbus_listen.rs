25f06f8f56ebeaaa04f171917a4a9bf2e25caf0a
Ben Boeckel <mathstuf@gmail.com>
2015-08-12 00:38:16 -0400

    dbus_listen: also look for method calls

--- src/dbus_listen.rs
+++ src/dbus_listen.rs
@@ -1,5 +1,5 @@
 extern crate dbus;
-use self::dbus::ConnectionItem;
+use self::dbus::{ConnectionItem, Message};
 
 use std::vec::Vec;
 
@@ -21,20 +21,23 @@ fn match_info(info: &SignalInfo, expect: &SignalInfo) -> bool {
     cmp_option(&expect.member, &info.member)
 }
 
+fn handle_message<Ctx>(msg: Message, map: &CallbackMap<Ctx>, ctx: &Ctx) -> () {
+    let (_, p, o, m) = msg.headers();
+    let info = SignalInfo { path: p, object: o, member: m };
+
+    for cb_item in (*map).iter() {
+        let (ref expect, ref cb) = *cb_item;
+
+        if match_info(&info, expect) {
+            cb(&info, ctx);
+        }
+    }
+}
+
 pub fn match_signal<Ctx>(item: ConnectionItem, map: &CallbackMap<Ctx>, ctx: &Ctx) -> () {
     match item {
-        ConnectionItem::Signal(s) => {
-            let (_, p, o, m) = s.headers();
-            let info = SignalInfo { path: p, object: o, member: m };
-
-            for cb_item in (*map).iter() {
-                let (ref expect, ref cb) = *cb_item;
-
-                if match_info(&info, expect) {
-                    cb(&info, ctx);
-                }
-            }
-        },
+        ConnectionItem::MethodCall(m) => handle_message(m, map, ctx),
+        ConnectionItem::Signal(s) => handle_message(s, map, ctx),
         _ => ()
     }
 }
