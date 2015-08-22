b1e3fbf18907d1b359f3a16fb283f006560a055a
Ben Boeckel <mathstuf@gmail.com>
2015-08-21 20:54:30 -0400

    error: replace with a struct
    No need for an enum.

--- src/error.rs
+++ src/error.rs
@@ -4,11 +4,9 @@ use self::config::error::ConfigError;
 use std::fmt;
 use std::error::Error;
 
-use self::WrapConfigError::CError;
-
 #[derive(Debug)]
-pub enum WrapConfigError {
-    CError(String),
+pub struct WrapConfigError {
+    desc: String,
 }
 
 impl fmt::Display for WrapConfigError {
@@ -20,15 +18,15 @@ impl fmt::Display for WrapConfigError {
 
 impl Error for WrapConfigError {
     fn description(&self) -> &str {
-        match *self {
-            CError(ref desc) => &(*desc)[..],
-        }
+        &self.desc[..]
     }
 }
 
 impl From<ConfigError> for WrapConfigError {
     fn from(config: ConfigError) -> WrapConfigError {
         let default_desc = format!("unknown parse error");
-        CError(format!("{:?} {}: {}", config.kind, config.desc, config.detail.unwrap_or(default_desc)))
+        WrapConfigError {
+            desc: format!("{:?} {}: {}", config.kind, config.desc, config.detail.unwrap_or(default_desc)),
+        }
     }
 }
